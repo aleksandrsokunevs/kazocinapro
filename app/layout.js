dd2ad19d03badc4eb1b54b11a01759af3f8fc6bc
Aleksandrs <aleksandrs@okunevs.pro>
2025-07-12 22:35:17 +0300

    menbere iznemts bez kludam

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,5 @@
-import { Inter } from 'next/font/google'
+mport { Inter } from 'next/font/google'
 import './globals.css'
-
-// Fontu konfigurācija
-// Piezīme: 'Menbere' nav standarta Google fonts, tāpēc šī ir hipotētiska ielāde.
-// Ja tas nav pieejams caur `next/font`, būs jāizmanto <link> tags, kā HTML versijā.
-// Pagaidām atstājam kā ir, bet Next.js varētu mest kļūdu.
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -19,14 +14,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="lv" className={`${menbere.variable} ${inter.variable}`}>
-      <head>
-        {/* Alternatīva metode, ja 'next/font' nestrādā priekš Menbere */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Menbere:wght@100..700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-serif">{children}</body>
+    <html lang="lv" className={inter.variable}>
+      <body className="font-sans">{children}</body>
     </html>
   )
 }
